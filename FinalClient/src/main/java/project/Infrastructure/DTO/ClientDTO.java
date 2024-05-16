d593a56800e0593c325da626b278e8a305c8f2bb
cesarAncco <canccor@gmail.com>
2024-05-16 00:27:39 -0500

    Commit Client: Adding the new attributes for Client.

--- FinalClient/src/main/java/project/Infrastructure/DTO/ClientDTO.java
+++ FinalClient/src/main/java/project/Infrastructure/DTO/ClientDTO.java
@@ -9,5 +9,12 @@ import lombok.NoArgsConstructor;
 @Data
 public class ClientDTO {
     String customId;
+    // String name;
+    // String address;
+    // String email;
+    // String phone;
+    // String status;
+    // String dateCreate;
+    // String document;
     String clientType;
 }
