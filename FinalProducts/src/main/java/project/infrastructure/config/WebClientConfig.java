2a95bf56d5a1f3ab892442d7be9f5800b53470f2
valnaevis <vgoyzuetat@ulasalle.edu.pe>
2024-05-24 00:04:09 -0500

    Port Updatin yml del Config Server

--- FinalProducts/src/main/java/project/infrastructure/config/WebClientConfig.java
+++ FinalProducts/src/main/java/project/infrastructure/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package project.infrastructure.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +10,10 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @Configuration
 public class WebClientConfig {
-  @Value("${client.service.baseUrl}")
-  private String clientServiceBaseUrl;
-
   @Bean
   @LoadBalanced
   public WebClient.Builder loadBalancedWebClientBuilder() {
     return WebClient.builder()
-        .baseUrl(clientServiceBaseUrl); // URL base del servicio de cliente
+        .baseUrl("http://clientService"); // URL base del servicio de cliente
   }
 }
